370d41654b5299acde563e32a47e18026bf20691
willis-tan <willis.tan956@gmail.com>
2021-04-06 17:45:37 -0700

    updated q1-3 to correct formats

---
+++ dsc100/hw1/hw1-q3.sql
@@ -0,0 +1,24 @@
+-- set up the table
+create table MyRestaurants(
+    rname varchar,
+    food varchar,
+    distance int,
+    last_visit varchar,
+    rating int
+);
+
+/*
+Insert at least 5 tuples
+At least 1 restaurant I like
+At least 1 restaurant I do not like
+At least 1 restaurant as NULL
+*/
+insert into MyRestaurants values('in-n-out', 'burgers', 2, '2021-03-27', 1);
+insert into MyRestaurants values('burger king', 'burgers', 20, '2020-06-01', 0);
+insert into MyRestaurants values('subway', 'sandwiches', 10, '2021-03-27', 'NULL');
+insert into MyRestaurants values('savoy', 'fusion', 15, '2020-08-10', 1);
+insert into MyRestaurants values('baccali', 'hong kong diner', 25, '2021-03-01', 1);
+-- handle null values
+update MyRestaurants
+set rating = NULL
+where rating = 'NULL';
